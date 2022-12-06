bd8c7a3c618e685dcbc1b1bd275990b964a7bfbf
CyrineOsorio <cyrineosorio18@gmail.com>
2022-12-06 15:57:29 +0800

    started: Home page

--- rejay45app/components/Navbar.jsx
+++ rejay45app/components/Navbar.jsx
@@ -1,7 +1,34 @@
 import styles from "./Navbar.module.css"
+import React, {useState} from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
-   
+    const [show,setShow]=useState(true)
+    return (
+        <div>
+            <div className={styles.container}>
+                <img src="/images/logo.png" alt="rejay45logo" className={styles.rejay45logo} />
+                <div className={styles.menubars}>
+                    <div className={styles.container1}>
+                        <Link href="#Home" className={styles.links1}>Home</Link>
+                        <Link href="#About" className={styles.links2}>About</Link>
+                        <Link href="#Services" className={styles.links3}>Services</Link>
+                        <Link href="#Contact" className={styles.links4}>Contact</Link>
+                        <button className={styles.button1} onClick={()=>setShow(!show)}><img src="/images/hamburger.png" alt="hamborgerButton" className={styles.hamborgerButton} /></button>
+                    </div>
+                    
+                </div>
+                
+            </div>
+            {show?<div className={styles.container2}>
+                <div className={styles.container2a}>
+                    <Link href="#Home" className={styles.links5}>Home</Link>
+                    <Link href="#About" className={styles.links6}>About</Link>
+                    <Link href="#Services" className={styles.links7}>Services</Link>
+                    <Link href="#Contact" className={styles.links8}>Contact</Link>
+                </div>
+            </div>:null}
+        </div>)
 };
 
 export default Navbar;
--- rejay45app/pages/index.js
+++ rejay45app/pages/index.js
@@ -4,39 +4,23 @@ import Navbar from '../components/navbar'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import React, {useState} from 'react'
-import { motion } from "framer-motion"
 
 export default function Home() {
-    const [show,setShow]=useState(true)
+
     return (
-        <div>
+        <div style={{backgroundColor: '#1B1B1B', height: '100vh'}}>
             <Head>
                 <title> Home: Rejay45 </title> 
                 <link rel="shortcut icon" href="/images/logo.png" type="image/x-icon" />
             </Head> 
-            
-            <div className={styles.container}>
-                <img src="/images/logo.png" alt="rejay45logo" className={styles.rejay45logo} />
-                <div className={styles.menubars}>
-                    <div className={styles.container1}>
-                        <Link href="#Home" className={styles.links1}>Home</Link>
-                        <Link href="#About" className={styles.links2}>About</Link>
-                        <Link href="#Services" className={styles.links3}>Services</Link>
-                        <Link href="#Contact" className={styles.links4}>Contact</Link>
-                        <button className={styles.button1} onClick={()=>setShow(!show)}><img src="/images/hamburger.png" alt="hamborgerButton" className={styles.hamborgerButton} /></button>
-                    </div>
+            <div className={styles.cont}>
+                <Navbar/>
+            </div>
+            <div className={styles.body}>
+                <div className={styles.container}>
                     
                 </div>
-                
             </div>
-            {show?<div className={styles.container2}>
-                <div className={styles.container2a}>
-                    <Link href="#Home" className={styles.links5}>Home</Link>
-                    <Link href="#About" className={styles.links6}>About</Link>
-                    <Link href="#Services" className={styles.links7}>Services</Link>
-                    <Link href="#Contact" className={styles.links8}>Contact</Link>
-                </div>
-            </div>:null}
         </div>)
         
 }

3a224ca5c8cbf03e76ebbc669701bcf135be77d3
CyrineOsorio <cyrineosorio18@gmail.com>
2022-12-07 08:53:04 +0800

    Front End Complete

--- rejay45app/pages/index.js
+++ rejay45app/pages/index.js
@@ -1,13 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Navbar from '../components/navbar'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link'
 import React, {useState} from 'react'
 import HomePage from '../components/HomePage'
-import AboutPage from '../components/AboutPage'
-import ServicesPage from '../components/ServicesPage'
-import ContactPage from '../components/ContactPage'
 
 export default function Home() {
 
@@ -19,39 +13,7 @@ export default function Home() {
             </Head> 
            
             <div className={styles.cont}>
-                <Navbar/>
-            </div>
-            <div className={styles.body}>
-                <div className={styles.containerServices}>
-                    <div className={styles.labelbox}>
-                        <div className={styles.label}>
-                            <h1>What We <span style={{color:'#1B1B1B'}}>DO.</span></h1>
-                            <div className={styles.linya}></div>
-                          
-                        </div>
-                    </div>
-                    <div className={styles.contleft}>
-                        <div className={styles.pic1}>
-                            <img src="/images/11.png" alt="" className={styles.img1} />
-                            <h3>Digital Marketing</h3> 
-                        </div>
-                        <div className={styles.pic1}>
-                            <img src="/images/12.png" alt="" className={styles.img1} />
-                            <h3>Brand Labeling</h3> 
-                        </div>
-                        <div className={styles.pic1}>
-                            <img src="/images/13.png" alt="" className={styles.img1} />
-                            <h3>Web Development</h3> 
-                        </div>
-                        <div className={styles.pic1}>
-                            <img src="/images/14.png" alt="" className={styles.img1} />
-                            <h3>UI/UX Designing</h3> 
-                        </div>
-                        
-                    </div>
-                    
-                </div>
-                
+                <HomePage/>
             </div>
         </div>)
         

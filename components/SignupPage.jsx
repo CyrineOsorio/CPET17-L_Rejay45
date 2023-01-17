4246a1b8491a6b2b442fb734d1dd9011a347c0eb
CyrineOsorio <cyrineosorio18@gmail.com>
2023-01-17 18:46:41 +0800

    ui only

---
+++ components/SignupPage.jsx
@@ -0,0 +1,14 @@
+import styles from "./SignupPage.module.css"
+import React from "react"
+import Link from "next/link"
+import Image from "next/image"
+
+const SignupPage = () => {
+    return ( 
+    <div>
+        
+    </div>
+    )
+};
+
+export default SignupPage;

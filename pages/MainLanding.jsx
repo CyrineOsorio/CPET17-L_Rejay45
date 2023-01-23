c34313f51a9ba3ad9e14ef1e7ab08d9f5c41d63b
unknown <jhonaspher.hinacay@gsfe.tupcavite.edu.ph>
2023-01-23 19:03:54 +0800

    Add FIles

---
+++ pages/MainLanding.jsx
@@ -0,0 +1,80 @@
+import styles from "../components/MainLandingPage.module.css"
+import React from "react"
+import Image from "next/image"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
+export default function MainLanding() {
+    
+    return (
+    <div className={styles.container}>
+    <ToastContainer />
+        <div className={styles.logo}>
+            <Image src="/images/rejayselcam.png" alt="rejay45logo" className={styles.rejay45logo} width={1000} height={100} />
+            <div className={styles.menubars}>
+                {/* <a href="http://localhost:3000/display" className={styles.logoutbutton}>Motion</a> */}
+                <a href="http://localhost:7000/logout" className={styles.logoutbutton}>Logout</a>
+            </div>
+        </div>
+        
+        <div className={styles.body}>
+            <div className={styles.body1}>
+                <div className={styles.usercont}>
+                    <div className={styles.headbanner}>
+                        <p>Welcome!</p>
+                        <a href="/ResetPass" className={styles.resetpasswordlink}>Reset Password</a>
+                    </div>
+                </div>
+
+
+                <div className={styles.filesfiles}>
+                    <div className={styles.lagayan}>
+                        <div className={styles.photocontainer}>
+                           
+                                <div className={styles.body1_a}>
+                                    <Image src="/images/logingal.png" alt="logingal" className={styles.logingal} width={800} height={500} />
+                                    <div className={styles.paragraph}>
+                                        <p>
+                                             WebCam Motion Detector in Python
+                                        </p>
+                                        <p>
+                                            This python program will allow you to detect motion and also store the date & time detected.
+                                        </p> 
+                                        <br />
+                                        <a href="http://localhost:3000/display" className={styles.tryme_btn}>Motion</a>
+                                          
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        
+    </div>
+ );
+}
+
+export async function getStaticProps() {
+    // Fetch data from the server
+    const res = await fetch('http://localhost:7000/MainLanding');
+  
+    // Get the json response
+    const data = await res.json();
+    
+    // If user was not logged in, go to login page
+    if ( data.is_logged_in == false ) {
+      return {
+        redirect: {
+          destination: "/Login",
+          permanent: false,
+        },
+        props: {},
+      };
+    }
+    // If user was logged in, redirect to this current page
+    return { props: {} }
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,30 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import HomePage from '../components/HomePage'
-import LoginPage from '../components/LoginPage'
 import MainLanding from './MainLanding'
 
 export default function Home() {
 
-    return (
-        <div style={{backgroundColor: '#1B1B1B', height: '100vh'}}>
-            <Head>
-                <title> Home: Rejay45 </title> 
-                <link rel="shortcut icon" href="/images/logo.png" type="image/x-icon" />
-            </Head> 
-           
-            <div>
-               <HomePage/>
-            </div>
-        </div>)
-        
-}
+    return ( <
+        div style = {
+            { backgroundColor: '#1B1B1B', height: '100vh' } } >
+        <
+        Head >
+        <
+        title > Home: Rejay45 < /title>  <
+        link rel = "shortcut icon"
+        href = "/images/logo.png"
+        type = "image/x-icon" / >
+        <
+        /Head> 
 
+        <
+        div >
+        <
+        HomePage / >
+        <
+        /div> <
+        /div>)
+
+    }
